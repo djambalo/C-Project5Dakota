7dd39e24484665443fe204cbb2d8926866629051
Dakota <dakotagarrison.14@gmail.com>
2023-11-02 11:41:52 -0500

    added display attributes to the model to change display name

--- Models/Course.cs
+++ Models/Course.cs
@@ -5,11 +5,15 @@ namespace CourseCatalog.Models
     public class Course
     {
         public int Id { get; set; }
+        [Display(Name = "Course Name")]
         public string? CourseName { get; set; }
+        [Display(Name = "Course Description")]
         public string? CourseDescription { get; set; }
+        [Display(Name = "Room Number")]
         public int RoomNumber { get; set; }
 
         [DataType(DataType.Time)]
+        [Display(Name = "Start Name")]
         public TimeOnly StartTime { get; set; }
     }
 }
